c5ead62ae54138387faf34c71ac05bf9bec46478
Sagar rai <notdoneyet2008@gmail.com>
2026-05-27 23:46:53 +0530

    Adding two numbers

---
+++ Day 3/addnum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cout << "Enter two numbers: \n";
+    cin >> x >> y;
+    z = x + y;
+    cout << "Addition is:\n"
+         << z;
+    return 0;
+}
