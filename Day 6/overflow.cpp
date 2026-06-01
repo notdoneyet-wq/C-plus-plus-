51ecaa4c1ab48182fdfc6cac894e45ef74d6db27
Sagar rai <notdoneyet2008@gmail.com>
2026-06-01 23:38:36 +0530

    Day 6

---
+++ Day 6/overflow.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char x = 128;
+    x++;
+    cout << int(x) << endl;
+
+    char y = -128;
+    y--;
+    cout << int(y) << endl;
+
+    int z = INT_MAX;
+    z ++;
+    cout << z << endl;
+    return 0;
+}
