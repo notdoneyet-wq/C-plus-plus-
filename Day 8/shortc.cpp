fc737fa7db1b91f69df2eba659b1fdfbcc875872
Sagar rai <notdoneyet2008@gmail.com>
2026-06-03 00:03:25 +0530

    Day 8

---
+++ Day 8/shortc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 5, i = 7;
+
+    if (a < b && ++i <= b)
+    {
+    }
+    cout << i << endl;
+    // ---------------------------
+    if (a < b || ++i <= b)
+    {
+    }
+    cout << i << endl;
+    return 0;
+}
